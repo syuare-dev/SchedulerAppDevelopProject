2f008c8dc680f02b32cb9ebe405d83e93e1c4589
syuare-dev <tachunsa13@gmail.com>
2025-05-19 15:29:52 +0900

    Init: 초기 생성

---
+++ src/test/java/com/example/schedulerapp/SchedulerAppDevelopProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.schedulerapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerAppDevelopProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
