ec8ad3d07c28aa5a44298a77cbec4c89c480e85c
syuare-dev <tachunsa13@gmail.com>
2025-05-20 16:08:56 +0900

    Update: user_id에 NOT NULL 속성 추가
    - user_id는 NULL이 되면 안되기 때문에 NOT NULL 속성 추가
    - user_id 컬럼 추가 즉시에는 NULL 값이 될 수 있고 속성은 NOT NULL인데, 값이 NULL이 되면 오류가 발생 > 디폴트 값 0으로 설정하여 컬럼 생성
    - NOT NULL로 설정했기 때문에 UPDATE에서의 NULL 일 경우 조건이었던 WHERE 구문 삭제

--- schedule.sql
+++ schedule.sql
@@ -6,8 +6,8 @@ CREATE TABLE schedules (
     username VARCHAR(100) NOT NULL COMMENT '작성 유저명',
     title VARCHAR(255) NOT NULL COMMENT '할 일 제목',
     contents LONGTEXT COMMENT '할 일 내용',
-    created_at DATE NOT NULL COMMENT '일정 생성일',
-    modified_at DATE NOT NULL COMMENT '일정 수정일'
+    created_at DATETIME NOT NULL COMMENT '일정 생성일',
+    modified_at DATETIME NOT NULL COMMENT '일정 수정일'
 );
 
 /* users(작성자) 테이블 생성 */
@@ -15,32 +15,33 @@ CREATE TABLE users (
     id BIGINT AUTO_INCREMENT PRIMARY KEY COMMENT '유저 식별자',
     name VARCHAR(100) NOT NULL COMMENT '작성 유저 이름',
     email VARCHAR(255) COMMENT '작성 유저 이메일',
-    created_at DATE COMMENT '등록일',
-    modified_at DATE COMMENT '수정일'
+    created_at DATETIME COMMENT '등록일',
+    modified_at DATETIME COMMENT '수정일'
 );
 
 /* schedules > user_id 컬럼 추가 */
 ALTER TABLE schedules
-ADD COLUMN user_id BIGINT COMMENT '유저 식별자 ID' AFTER id;
+ADD COLUMN user_id BIGINT NOT NULL DEFAULT 0 COMMENT '유저 식별자 ID' AFTER id;
 
 /* users > name, created_at, modified_at 데이터 값을 schedules 테이블에서 가져와서 데이터 삽입 */
 INSERT INTO users(name, created_at, modified_at)
-SELECT DISTINCT schedules.username, schedules.created_at, schedules.modified_at
-FROM schedules;
+SELECT schedules.username, schedules.created_at, schedules.modified_at
+FROM schedules
+GROUP BY schedules.username, schedules.created_at, schedules.modified_at;
 
 
 /* schedules 와 users 테이블 JOIN > s.user_id 에 u.id 값 없데이트 */
-UPDATE schedules s JOIN users u ON s.username = u.name
-SET s.user_id = u.id
-WHERE s.user_id IS NULL;
+UPDATE schedules s
+    JOIN users u ON s.username = u.name
+SET s.user_id = u.id;
 
 /* user_id 외래 키(FK) 선언 */
 ALTER TABLE schedules
-MODIFY user_id BIGINT NOT NULL,
     ADD CONSTRAINT FK_schedules_users
         FOREIGN KEY (user_id) REFERENCES users(id)
             ON DELETE RESTRICT
             ON UPDATE CASCADE;
+
 
 /* users > password 컬럼 추가 */
 ALTER TABLE users
