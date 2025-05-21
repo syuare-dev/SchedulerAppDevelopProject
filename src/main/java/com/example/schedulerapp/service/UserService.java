d5c77ff14489a79c8a93af3d5b3f0e9647725db8
syuare-dev <tachunsa13@gmail.com>
2025-05-21 19:19:53 +0900

    Add: 유저 조회 API 구현
    Service 레이어:
    - 일정 조회 기능 구현
     -- 전체 유저 조회: findAllUser()
     -- 선택 유저 조회(id 기준): findByIdUser()

--- src/main/java/com/example/schedulerapp/service/UserService.java
+++ src/main/java/com/example/schedulerapp/service/UserService.java
@@ -1,8 +1,15 @@
 package com.example.schedulerapp.service;
 
 import com.example.schedulerapp.dto.userDto.UserSignUpResponseDto;
+import com.example.schedulerapp.dto.userDto.UserTimeIncludeResponseDto;
+
+import java.util.List;
 
 public interface UserService {
 
     UserSignUpResponseDto signUp(String name, String email);
+
+    List<UserTimeIncludeResponseDto> findAllUsers();
+
+    UserTimeIncludeResponseDto findByIdUser(Long id);
 }
--- src/main/java/com/example/schedulerapp/service/UserServiceJPA.java
+++ src/main/java/com/example/schedulerapp/service/UserServiceJPA.java
@@ -1,10 +1,13 @@
 package com.example.schedulerapp.service;
 
 import com.example.schedulerapp.dto.userDto.UserSignUpResponseDto;
+import com.example.schedulerapp.dto.userDto.UserTimeIncludeResponseDto;
 import com.example.schedulerapp.entity.User;
 import com.example.schedulerapp.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -20,5 +23,18 @@ public class UserServiceJPA implements UserService {
         User savedUser = userRepository.save(user);
 
         return new UserSignUpResponseDto(savedUser.getId(), savedUser.getName(), savedUser.getEmail());
+    }
+
+    @Override
+    public List<UserTimeIncludeResponseDto> findAllUsers() {
+        return userRepository.findAll().stream().map(UserTimeIncludeResponseDto::toDto).toList();
+    }
+
+    @Override
+    public UserTimeIncludeResponseDto findByIdUser(Long id) {
+
+        User findUser = userRepository.findByIdOrElseThrow(id);
+
+        return new UserTimeIncludeResponseDto(findUser.getId(), findUser.getName(), findUser.getEmail(), findUser.getCreatedAt(), findUser.getModifiedAt());
     }
 }
