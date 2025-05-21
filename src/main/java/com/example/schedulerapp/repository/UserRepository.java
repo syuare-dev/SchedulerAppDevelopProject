edc35fc43db225d6f5fac1834b678fb27b2f73b8
syuare-dev <tachunsa13@gmail.com>
2025-05-21 19:18:50 +0900

    Add: 유저 조회 API 구현
    Repository 레이어:
    - 선택 유저 찾기(id 기준) 예외 처리를 인터페이스에서 구현

--- src/main/java/com/example/schedulerapp/repository/UserRepository.java
+++ src/main/java/com/example/schedulerapp/repository/UserRepository.java
@@ -2,6 +2,12 @@ package com.example.schedulerapp.repository;
 
 import com.example.schedulerapp.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    default User findByIdOrElseThrow(Long id) {
+        return findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Does not exist id = " + id));
+    }
 }
