1425b1690bcffe19e804454cf93eee8cea2a9c4d
syuare-dev <tachunsa13@gmail.com>
2025-05-22 19:41:55 +0900

    Correct: 사용되지 않는 어노테이션 삭제

--- src/main/java/com/example/schedulerapp/dto/userDto/UpdateUserRequestDto.java
+++ src/main/java/com/example/schedulerapp/dto/userDto/UpdateUserRequestDto.java
@@ -1,11 +1,9 @@
 package com.example.schedulerapp.dto.userDto;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Column;
 import lombok.Getter;
 
 @Getter
-@JsonIgnoreProperties(ignoreUnknown = false)
 public class UpdateUserRequestDto {
 
     @Column(nullable = false)
