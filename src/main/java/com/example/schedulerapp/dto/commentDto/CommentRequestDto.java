fee4633bb518fc79cd1df8d431c7c86fcfafb6ef
syuare-dev <tachunsa13@gmail.com>
2025-05-24 12:41:50 +0900

    Refactor: 댓글 생성 기능 리팩토링 > 저장되는 username 을 로그인한 유저 정보로 저장
    - 요청 DTO
    > - username 속성 삭제
    
    - Controller 레이어
    > - updateByMyInfo 매개변수로 HttpServletRequest 추가
    > - 서블릿의 getAttribute 를 통해 userId 값 얻은 후
    > - 서비스 레이어에 요청 DTO 와 함께 전달
    
    - Service 레이어
    > - 매개변수로 받은 유저 식별자 Id 와 로그인되어 있는 유저의 id와 일치 여부 비교
    > - 댓글 생성 시 요청 DTO 의 username 값 대신 로그인 유저의 username 을 저장하도록 수정

--- src/main/java/com/example/schedulerapp/dto/commentDto/CommentRequestDto.java
+++ src/main/java/com/example/schedulerapp/dto/commentDto/CommentRequestDto.java
@@ -1,20 +1,16 @@
 package com.example.schedulerapp.dto.commentDto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.hibernate.validator.constraints.Length;
 
 @Getter
+@AllArgsConstructor
 public class CommentRequestDto {
 
     @NotNull
-    @Length(min = 1, max = 100)
+    @Length(min = 1, max = 255)
     private final String comment;
 
-    private final String username;
-
-    public CommentRequestDto(String comment, String username) {
-        this.comment = comment;
-        this.username = username;
-    }
 }
